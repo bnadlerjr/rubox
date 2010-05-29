8c7aceb13659c78aa882fc12da24c822ff9bf264
Bob Nadler <bob@Bob-Nadlers-MacBook-Pro.local>
2010-05-29 16:34:11 -0400

    Initial commit.

---
+++ lib/rubox.rb
@@ -0,0 +1,27 @@
+require "net/http"
+
+class Rubox
+  def initialize(api_key)
+    @api_key = api_key
+    @base_url = "https://www.box.net/api/1.0/rest?"
+  end
+
+  def method_missing(name, *args)
+    if args && args.count != 0
+      params = "&#{get_querystring(args[0])}"
+    else
+      params = ''
+    end
+    http_get("#{@base_url}action=#{name}&api_key=#{@api_key}#{params}")
+  end
+
+  private
+
+  def http_get(url)
+    Net::HTTP.get_response(URI.parse(url)).body.to_s
+  end
+
+  def get_querystring(args)
+    args.map { |k, v| "#{k}=#{v}" }.join('&')
+  end
+end
---
+++ test/test_rubox.rb
@@ -0,0 +1,46 @@
+require "rubygems"
+require "test/unit"
+require "mocha"
+require "rubox"
+
+class TestRubox < Test::Unit::TestCase
+  URL_BASE = "https://www.box.net/api/1.0/rest?"
+
+  def setup
+    @rubox = Rubox.new("rrc1d3ntb53tt6b2vhail6rdtrsxov3v")
+  end
+
+  def test_can_initialize
+    assert_not_nil @rubox
+  end
+
+  def test_can_create_querystring
+    args = {
+      :action => 'get_auth_token',
+      :api_key => 'rrc1d3ntb53tt6b2vhail6rdtrsxov3v',
+      :ticket => 'udd863k39gn9mioc6ym2c6erbqm8q'
+    }
+
+    result = @rubox.instance_eval { get_querystring(args) }
+    assert(result =~ /action=get_auth_token/, 'action')
+    assert(result =~ /api_key=rrc1d3ntb53tt6b2vhail6rdtrsxov3v/, 'api_key')
+    assert(result =~ /ticket=udd863k39gn9mioc6ym2c6erbqm8q/, 'ticket')
+  end
+
+  def test_can_create_ticket_request
+    url = URL_BASE + 
+      "action=get_ticket&api_key=rrc1d3ntb53tt6b2vhail6rdtrsxov3v"
+
+    @rubox.expects(:http_get).with(url)
+    @rubox.get_ticket
+  end
+
+  def test_can_create_auth_token_request
+    url = URL_BASE +
+      "action=get_auth_token&api_key=rrc1d3ntb53tt6b2vhail6rdtrsxov3v&" +
+      "ticket=udd863k39gn9mioc6ym2c6erbqm8q"
+
+    @rubox.expects(:http_get).with(url)
+    @rubox.get_auth_token(:ticket => "udd863k39gn9mioc6ym2c6erbqm8q")
+  end
+end
