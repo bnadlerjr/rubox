a03e073b94b447aa1f88bb2f4423fa06a4b1f597
Bob Nadler <bob@Bob-Nadlers-MacBook-Pro.local>
2010-05-30 13:25:59 -0400

    Refactored architecture for easier testing.
    * Moved Client class to a separate file in /lib
    * Added VERSION
    * Moved API related tests into test_parser
    * Moved request generation tests into test_client

--- lib/rubox.rb
+++ lib/rubox.rb
@@ -1,37 +1,9 @@
-require "net/http"
+require "rubox/client"
 require "rubox/user"
 require "rubox/folder"
 require "rubox/file"
 require "rubox/parser"
 
 module Rubox
-  class Client
-    def initialize(api_key)
-      @api_key = api_key
-      @base_url = "https://www.box.net/api/1.0/rest?"
-    end
-
-    def method_missing(name, *args)
-      if args && args.count != 0
-        params = "&#{get_querystring(args[0])}"
-      else
-        params = ''
-      end
-
-      response = 
-        http_get("#{@base_url}action=#{name}&api_key=#{@api_key}#{params}")
-
-      Parser.new(response).send(name)
-    end
-
-    private
-
-    def http_get(url)
-      Net::HTTP.get_response(URI.parse(url)).body.to_s
-    end
-
-    def get_querystring(args)
-      args.map { |k, v| "#{k}=#{v}" }.join('&')
-    end
-  end
+  VERSION = 0.1
 end
